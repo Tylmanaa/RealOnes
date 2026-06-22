9d9df2a9e5cfb0da51d7d069c8705fd02dddb6b4
Alexander Tylman <justlogpong@gmail.com>
2026-06-22 13:03:43 +0300

    Update logs.py

--- logs.py
+++ logs.py
@@ -1,5 +1,5 @@
 try:
-    # опасный код, который может сломаться (например, попытка открыть файл)
+    
     mistakes=[] 
     with open('server.log', 'r') as file:
         for line in file:
@@ -8,13 +8,12 @@ try:
                 
 
 
-    # Здесь file ведет себя как список строк, по которому можно пройтись циклом for
 
         answer= f"Общее количество ошибок, {len(mistakes)}"
     
         with open('report.txt', 'w') as report_file:
             for log in mistakes:
-                report_file.write(log) # \n нужен для переноса строки
+                report_file.write(log) 
             report_file.write(answer)
 
 except FileNotFoundError:
